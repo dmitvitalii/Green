abd05951e5d9b022fdbbf15e2ba17f885c7689d7
Vitalii Dmitriev <dmitvitalii@gmail.com>
2019-06-08 18:37:04 +0300

    Use constructor injection for users

--- src/main/kotlin/me/dmitvitalii/green/user/controller/UserRestController.kt
+++ src/main/kotlin/me/dmitvitalii/green/user/controller/UserRestController.kt
@@ -1,17 +1,13 @@
 package me.dmitvitalii.green.user.controller
 
 import me.dmitvitalii.green.user.model.UserRepository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class UserRestController {
-
-    @Autowired
-    lateinit var repository: UserRepository
+class UserRestController(val repository: UserRepository) {
 
     // TODO: plant in rooms inside buildings
     @GetMapping("/users")
@@ -23,6 +19,5 @@ class UserRestController {
     }
 
     @GetMapping("/users/{id}")
-    fun getUser(@PathVariable(name = "id") id: Long) = repository.findById(id)
-            ?: throw NoSuchElementException("Cannot find any rooms with id $id")
+    fun getUser(@PathVariable(name = "id") id: Long) = repository.findById(id).orElseThrow()
 }
