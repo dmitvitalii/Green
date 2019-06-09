a7c21e849ab7ae159ddd31e2fce7f9070209714b
Vitalii Dmitriev <dmitvitalii@gmail.com>
2019-06-09 23:33:41 +0300

    Remove useless parameters from getRooms()

--- src/main/kotlin/me/dmitvitalii/green/room/controller/RoomRestController.kt
+++ src/main/kotlin/me/dmitvitalii/green/room/controller/RoomRestController.kt
@@ -3,7 +3,6 @@ package me.dmitvitalii.green.room.controller
 import me.dmitvitalii.green.room.model.RoomRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,12 +11,7 @@ class RoomRestController(val repository: RoomRepository) {
 
     // TODO: plant in rooms inside buildings
     @GetMapping("/rooms")
-    fun getRooms(
-            @RequestParam(name = "index", required = false) index: Int?,
-            @RequestParam(name = "number", required = false) number: Int?
-    ) {
-        repository.findAll()
-    }
+    fun getRooms() = repository.findAll()
 
     @GetMapping("/rooms/{id}")
     fun getRoom(@PathVariable(name = "id") id: Long) = repository.findById(id).get()
