c4e2534ec118cf6b95320532ebe92750548b893a
Vitalii Dmitriev <dmitvitalii@gmail.com>
2019-08-28 15:53:51 +0300

    Add an api for plant irrigation

--- src/main/kotlin/me/dmitvitalii/green/plant/controller/PlantsRestController.kt
+++ src/main/kotlin/me/dmitvitalii/green/plant/controller/PlantsRestController.kt
@@ -1,13 +1,21 @@
 package me.dmitvitalii.green.plant.controller
 
+import me.dmitvitalii.green.plant.model.Plant
 import me.dmitvitalii.green.plant.model.PlantRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class PlantsRestController(val repository: PlantRepository) {
 
+    @ResponseBody
     @GetMapping("/plant/{id}")
     fun getPlant(@PathVariable(name = "id") id: Long) = repository.findById(id).get()
+
+    @PatchMapping("/plant/{id}/irrigate")
+    fun irrigatePlant(@PathVariable(name = "id") id: Long, @RequestBody plant: Plant) {
+        val changePlant = repository.findById(id).get()
+        val newPlant = changePlant.copy(irrigated = plant.irrigated)
+        repository.deleteById(id)
+        repository.save(newPlant)
+    }
 }
