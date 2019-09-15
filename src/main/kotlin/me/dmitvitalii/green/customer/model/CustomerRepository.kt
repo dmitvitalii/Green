168cebe9e922b98e486f9e51ba10543473e1fd62
Vitalii Dmitriev <dmitvitalii@gmail.com>
2019-09-15 22:52:07 +0300

    Mark repositories as @Transactional @Repository

--- src/main/kotlin/me/dmitvitalii/green/customer/model/CustomerRepository.kt
+++ src/main/kotlin/me/dmitvitalii/green/customer/model/CustomerRepository.kt
@@ -1,14 +1,16 @@
 package me.dmitvitalii.green.customer.model
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
+@Transactional
+@Repository
 interface CustomerRepository : CrudRepository<Customer, Long> {
-
     override fun findById(id: Long): Optional<Customer>
 
     fun findAllById(parentId: Long): Collection<Customer>
 
     fun save(element: Customer): Long
-
 }
--- src/main/kotlin/me/dmitvitalii/green/room/model/RoomRepository.kt
+++ src/main/kotlin/me/dmitvitalii/green/room/model/RoomRepository.kt
@@ -1,11 +1,13 @@
 package me.dmitvitalii.green.room.model
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 
+@Transactional
+@Repository
 interface RoomRepository : CrudRepository<Room, Long> {
-
     override fun findById(id: Long): Optional<Room>
-
 }
