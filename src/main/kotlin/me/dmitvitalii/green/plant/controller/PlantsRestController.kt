212425f85b5ae50abe5335b9536bb96076012319
Vitalii Dmitriev <dmitvitalii@gmail.com>
2019-11-05 21:51:35 +0300

    Return not found if there is no such plant

--- src/main/kotlin/me/dmitvitalii/green/plant/controller/PlantsRestController.kt
+++ src/main/kotlin/me/dmitvitalii/green/plant/controller/PlantsRestController.kt
@@ -2,6 +2,8 @@ package me.dmitvitalii.green.plant.controller
 
 import me.dmitvitalii.green.plant.model.Plant
 import me.dmitvitalii.green.plant.model.PlantRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -9,7 +11,14 @@ class PlantsRestController(val repository: PlantRepository) {
 
     @ResponseBody
     @GetMapping("plant/{id}")
-    fun getPlant(@PathVariable(name = "id") id: Long) = repository.findById(id).get()
+    fun getPlant(@PathVariable(name = "id") id: Long): ResponseEntity<Plant> {
+        val result = repository.findByIdOrNull(id)
+        return if (result == null) {
+            ResponseEntity.notFound().build<Plant>()
+        } else {
+            ResponseEntity.ok().body(result)
+        }
+    }
 
     @PatchMapping("plant/{id}/_irrigate")
     fun irrigatePlant(
